a5988bafec4b85450f6709e4b80bddfbf4b07f1a
Rasko Lazic <raskolazic@gmail.com>
2016-03-21 16:43:56 +0100

    first homework

--- OP1Vezbe/Domaci1.cpp
+++ OP1Vezbe/Domaci1.cpp
@@ -11,6 +11,9 @@ class Date {
 private:
     int day, month, year;
 public:
+    int monthLength[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    bool leap = false;
+
     Date() {
         day = 1;
         month = 1;
@@ -34,10 +37,10 @@ public:
     }
 
     int setDay(int value) {
-        int monthLength[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         if(this->year % 4 == 0) {
             monthLength[1]++;
+            this->leap = true;
         }
 
         if(value > 0 && value <=     monthLength[this->month - 1]) {
@@ -50,6 +53,23 @@ public:
             return 1;
         }
     }
+
+    int getDay() {
+        return this->day;
+    }
+
+    int getMonth() {
+        return this->month;
+    }
+
+    int getYear() {
+        return this->year;
+    }
+
+    int getMonthLength() {
+        return monthLength[this->month - 1];
+    }
+
 };
 
 class Domaci1: public Vezba {
@@ -76,5 +96,15 @@ public:
 
             validation = newDate.setDay(value);
         } while(validation);
+
+        cout << "Selected year is: " << newDate.getYear() << endl;
+
+        cout << "Selected month has: " << newDate.getMonthLength() << " days" << endl;
+
+        if(newDate.leap) {
+            cout << "Selected year is leap year." << endl;
+        } else {
+            cout << "Selected year is not leap year." << endl;
+        }
     }
 };
